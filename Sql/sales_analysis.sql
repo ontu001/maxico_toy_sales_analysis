75eb209cd7988d53a50924a4672ee23d1733e175
ontu001 <mdrohanurrahmanontu@gmail.com>
2025-06-30 12:39:25 +0600

    city and store location wise saless

--- Sql/sales_analysis.sql
+++ Sql/sales_analysis.sql
@@ -18,3 +18,29 @@ inner join `maxico_toy_sales.sales` as s on p.Product_ID = s.Product_ID
 group by 1
 order by 2 desc;
 
+
+
+
+
+
+
+
+-- Which cities have the highest sales?
+select
+  store.Store_City,
+  sum(sale.Units) as total_Sell
+from `maxico_toy_sales.stores` as store
+inner join `maxico_toy_sales.sales` as sale on store.Store_ID = sale.Store_ID
+group by 1
+order by 2 desc;
+
+
+-- Which store locations have the highest sales?
+select
+  store.Store_Location,
+  sum(sale.Units) as total_Sell
+from `maxico_toy_sales.stores` as store
+inner join `maxico_toy_sales.sales` as sale on store.Store_ID = sale.Store_ID
+group by 1
+order by 2 desc;
+
