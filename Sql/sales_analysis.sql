6b06a6ca50acdf04e323fa65a41baa1b7c115f76
ontu001 <mdrohanurrahmanontu@gmail.com>
2025-06-30 16:59:44 +0600

    Sales Analysis completed

--- Sql/sales_analysis.sql
+++ Sql/sales_analysis.sql
@@ -19,6 +19,27 @@ group by 1
 order by 2 desc;
 
 
+-- What is the monthly sales trend?
+
+select
+  extract(month from s.Date) as Month,
+  sum(s.Units) as Total_Order,
+  round(sum(s.Units * p.Product_Price),2) as Revenue
+from `maxico_toy_sales.sales` as s
+inner join `maxico_toy_sales.products` as p on s.Product_ID = p.Product_ID
+group by 1;
+
+
+
+-- What is the yearly sales trend?
+
+select
+  extract(year from s.Date) as Month,
+  sum(s.Units) as Total_Order,
+  round(sum(s.Units * p.Product_Price),2) as Revenue
+from `maxico_toy_sales.sales` as s
+inner join `maxico_toy_sales.products` as p on s.Product_ID = p.Product_ID
+group by 1;
 
 
 
@@ -43,4 +64,41 @@ from `maxico_toy_sales.stores` as store
 inner join `maxico_toy_sales.sales` as sale on store.Store_ID = sale.Store_ID
 group by 1
 order by 2 desc;
+
+
+
+
+
+-- What is the average order value per store?
+select
+  s.Store_Name,
+  round(sum(sale.Units * p.Product_Price) / count( distinct sale.Sale_ID)) as avg_order-Value
+from `maxico_toy_sales.stores` as s
+inner join `maxico_toy_sales.sales` as sale on s.Store_ID = sale.Store_ID
+inner join `maxico_toy_sales.products` as p on p.Product_ID = sale.Product_ID
+group by 1
+order by 2 desc;
+
+
+-- Which products have the highest profit margin?
+select 
+  Product_Name,
+  round(Product_Price - Product_Cost, 2) as Porfit_Mergin
+from `maxico_toy_sales.products`
+order by 2 desc;
+
+
+
+
+-- How do sales differ across product categories?
+select
+  p.Product_Category,
+  sum(s.Units) as Unit_Sold,
+  round(sum(s.Units * p.Product_Price),2) as Total_Revenue,
+  round(avg(p.Product_Price),2) as Avg_Selling_Price,
+  sum(s.Units * (p.Product_Price - p.Product_Cost)) as Total_Profit
+from `maxico_toy_sales.products` as p
+inner join `maxico_toy_sales.sales` as s on p.Product_ID = s.Product_ID
+group by 1;
+
 
